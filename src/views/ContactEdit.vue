748e4a75c14e2646f221571014d85a6e6eaa6e0f
tndquocB2012248 <quocb2012248@student.ctu.edu.vn>
2022-09-24 11:00:46 +0700

    Tao trang cap nhat lien he

---
+++ src/views/ContactEdit.vue
@@ -0,0 +1,82 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4 class="editText">Hiệu chỉnh Liên hệ</h4>
+            <ContactForm
+            :contact="contact"
+            @submit:contact="updateContact"
+            @delete:contact="deleteContact"
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm,
+    },
+    props: {
+        id: { type: String, required: true },
+    },
+    data() {
+        return {
+            contact: null,
+            message: "",
+        };
+    },
+    methods: {
+
+        async getContact(id) {
+            try {
+                this.contact = await ContactService.get(id);
+            } catch (error) {
+                console.log(error);
+                // Chuyển sang trang NotFound đồng thời giữ cho URL không đổi
+                this.$router.push({
+                    name: "notfound",
+                    params: {
+                        pathMatch: this.$route.path.split("/").slice(1)
+                    },
+                    query: this.$route.query,
+                    hash: this.$route.hash,
+                });
+            }
+        },
+
+        async updateContact(data) {
+            try {
+                await ContactService.update(this.contact._id, data);
+                this.message = "Liên hệ được cập nhật thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        async deleteContact() {
+            if (confirm("Bạn muốn xóa Liên hệ này?")) {
+                try {
+                    await ContactService.delete(this.contact._id);
+                    this.$router.push({ name: "contactbook" });
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        },
+    },
+    created() {
+        this.getContact(this.id);
+
+        this.message = "";
+    },
+};
+</script>
+
+<style scoped>
+    .editText {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+</style>
