6144921c966b72dc4d4b84bcacfd7ef0a25a7b12
tndquocB2012248 <quocb2012248@student.ctu.edu.vn>
2022-09-24 10:38:29 +0700

    Cai dat trang loi 404

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import router from "./router";
+
 
 createApp(App).use(router).mount("#app");
 
